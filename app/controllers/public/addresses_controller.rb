1bce9f6593e289d094e7d04bf9548b9d62759ba2
nsr0623r <nsr0623r@yahoo.co.jp>
2022-12-13 11:30:58 +0000

    public/address add

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -1,7 +1,32 @@
 class Public::AddressesController < ApplicationController
+  # 配送先登録/一覧画面
   def index
+    @address = Address.new
+    @addresses = Address.all
   end
 
+  # 配送先編集画面
   def edit
+  end
+
+  # 配送先の登録
+  def create
+    @address = Address.new(address_params)
+    @address.save!
+    redirect_to addresses_path
+  end
+
+  # 配送先の更新
+  def update
+  end
+
+  # 配送先の削除
+  def destroy
+  end
+
+  private
+
+  def address_params
+    params.require(:address).permit(:postal_code_string, :address, :naame)
   end
 end

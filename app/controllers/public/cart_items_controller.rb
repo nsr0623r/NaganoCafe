d4d517cb4658a361ed62b71274834a90d4dc9c90
nsr0623r <nsr0623r@yahoo.co.jp>
2022-12-14 08:18:29 +0000

    public/cart_item add

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,4 +1,37 @@
 class Public::CartItemsController < ApplicationController
+  # カート内商品
   def index
+    @cart_items = CartItem.all
   end
+
+  # カート内商品追加
+  def create
+    @cart_item = CartItem.new(cart_item_params)
+    @cart_item.customer_id = current_customer.id
+    if cart_item = current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id])
+       cart_item.number += params[:cart_item][:number].to_i cart_item.save
+       redirect_to cart_items_path
+    elsif @cart_item.save
+       redirect_to cart_items_path
+    else
+       render :index
+    end
+  end
+
+  # 商品単価(税込)
+  def with_tax_price
+    (price * 1.1).floor
+  end
+
+  # 小計
+  def subtotal
+    item.with_tax_price * amount
+  end
+
+  private
+
+  def cart_item_params
+    params.require(:cart_item).permit(:item_id, :amount)
+  end
+
 end
--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,12 +1,13 @@
 class Public::ItemsController < ApplicationController
-  # 商品一覧
+  # 顧客：商品一覧
   def index
     @items = Item.all
   end
 
-  # 商品詳細
+  # 顧客：商品詳細
   def show
     @item = Item.find(params[:id])
+    @cart_item = CartItem.new
   end
   
   private
